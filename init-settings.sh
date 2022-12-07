4fef26a82a937e783795b33d21fe6cbd6382b0c6
o2asdv <119470164+o2asdv@users.noreply.github.com>
2022-12-07 12:03:46 +0800

    Update init-settings.sh

--- init-settings.sh
+++ init-settings.sh
@@ -14,6 +14,23 @@ sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 uci set fstab.@global[0].check_fs=1
 uci commit fstab
 
-echo "112233445566" >> /root/XX
+# Add "/etc/rc.local" script
+cat <<'EOF'> /etc/rc.local
+#!/bin/bash
+
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
+ROOTFS_DISK=$(mount | grep -w '/' | grep 'ro' | awk {'print $1'})
+if [ ${ROOTFS_DISK} ] ; then 
+  fsck.ext4 -y ${ROOTFS_DISK}
+  mount -o remount rw /
+fi
+
+if [ -f "/data/start.sh" ]; then
+  /bin/bash /data/start.sh
+fi
+
+exit 0
+EOF
 
 exit 0
