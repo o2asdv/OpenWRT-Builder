aed13229ad7a8bbc4d38917abb11f72672ec3bf1
o2asdv <119470164+o2asdv@users.noreply.github.com>
2022-12-08 20:29:04 +0800

    Update init-settings.sh

--- init-settings.sh
+++ init-settings.sh
@@ -14,6 +14,42 @@ sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 uci set fstab.@global[0].check_fs=1
 uci commit fstab
 
+# Config network
+cat <<'EOF'> /etc/config/network
+config interface 'loopback'
+        option ifname 'lo'
+        option proto 'static'
+        option ipaddr '127.0.0.1'
+        option netmask '255.0.0.0'
+
+config globals 'globals'
+        option ula_prefix 'fdd0:fb32:9b0c::/48'
+        option packet_steering '1'
+
+config interface 'lan'
+        option type 'bridge'
+        option proto 'static'
+        option netmask '255.255.255.0'
+        option _orig_ifname 'eth0'
+        option _orig_bridge 'true'
+        option ifname 'eth0 eth1 eth2 eth3'
+        option stp '1'
+        option ipaddr '172.16.252.1'
+        option gateway '172.16.252.1'
+        option broadcast '172.16.252.255'
+        option dns '172.16.252.1'
+
+config interface 'wan'
+        option proto 'dhcp'
+        option _orig_ifname 'eth1'
+        option _orig_bridge 'false'
+        option ifname 'eth4'
+
+config interface 'vpn0'
+        option ifname 'tun0'
+        option proto 'none'
+EOF
+
 # Add "/etc/rc.local" script
 cat <<'EOF'> /etc/rc.local
 #!/bin/bash
